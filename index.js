9b90f7b74caaddb6b9173c2a9891bfe834205a5e
Eric Skaftason <skaftasoneric@gmail.com>
2024-10-09 16:47:10 -0400

    edited menu_renderer example to use arrays

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const callback_function = () => {
 
 import {Menu_Renderer} from './menu_renderer.js';
 
-const btns = [
-    {txt: 'AA', callback: callback_function},
-    {txt: 'BB', callback: callback_function},
+const btns = [ //txt property must be an array
+    {txt: ['AA'], callback: callback_function},
+    {txt: ['BB'], callback: callback_function},
     {txt: ['CC1', 'CC2', 'CC3'], callback: callback_function},
 ];
 const menu = new Menu_Renderer('Title 12345', 'Subtitle', 'version xyz', btns, 300, 600);
