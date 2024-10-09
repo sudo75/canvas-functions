f58049a9054a0903e71ed169a0d11895b5fc5c9d
Eric Skaftason <skaftasoneric@gmail.com>
2024-10-08 23:42:07 -0400

    modularized code

---
+++ index.js
@@ -0,0 +1,13 @@
+const callback_function = () => {
+    console.log('callback executed');
+}
+
+import {Menu_Renderer} from './menu_renderer.js';
+
+const btns = [
+    {txt: 'AA', callback: callback_function},
+    {txt: 'BB', callback: callback_function},
+    {txt: ['CC1', 'CC2', 'CC3'], callback: callback_function},
+];
+const menu = new Menu_Renderer('Title 12345', 'Subtitle', 'version xyz', btns, 300, 600);
+menu.init();
---
+++ menu_renderer.js
@@ -0,0 +1,59 @@
+import {Btn_Menu} from './btn_menu.js';
+
+class Menu_Renderer {
+    constructor(title, subtitle, version, btns, width, height) {
+        this.title = title;
+        this.subtitle = subtitle;
+        this.btns = btns;
+        this.font = {
+            title: {style: 'bold', size: 32, typeface: 'IBM Plex Serif'},
+            subtitle: {style: 'italic', size: 16, typeface: 'Arial'},
+            version: {style: 'bold', size: 16, typeface: 'Arial'}
+        };
+
+        this.version = version;
+        
+        this.canvas = document.querySelector('.canvas');
+        this.ctx = this.canvas.getContext('2d');
+
+        this.width = width;
+        this.height = height;
+    }
+    init() {
+        this.canvas.width = this.width;
+        this.canvas.height = this.height;
+
+        this.ctx.textAlign = 'center';
+        this.ctx.textBaseline = 'middle';
+    
+        //Title
+        const titleX = this.canvas.width / 2;
+        const titleY = 20 + this.font.title.size / 2;
+
+        this.ctx.font = `${this.font.title.style} ${this.font.title.size}px ${this.font.title.typeface}`;
+        this.ctx.fillText(this.title, titleX, titleY);
+
+        //Subtitle
+        const subtitleX = this.canvas.width / 2;
+        const subtitleY = 20 + titleY + this.font.subtitle.size / 2;
+
+        this.ctx.font = `${this.font.subtitle.style} ${this.font.subtitle.size}px ${this.font.subtitle.typeface}`;
+        this.ctx.fillText(this.subtitle, subtitleX, subtitleY);
+
+        //Version
+        this.ctx.textAlign = 'left';
+        const versionX = 10;
+        const versionY = this.canvas.height - this.font.version.size;
+
+        this.ctx.font = `${this.font.version.style} ${this.font.version.size}px ${this.font.version.typeface}`;
+        this.ctx.fillText(this.version, versionX, versionY);
+
+        //Menu
+        const menuY = subtitleY + this.font.subtitle.size + 10;
+        const menu = new Btn_Menu(this.canvas, this.ctx, this.btns, 10, menuY);
+        menu.btn_dimensions.width = this.canvas.width - 20; //margin of 10 on each side
+        menu.init();
+    }
+}
+
+export { Menu_Renderer };
