ebd505f7a069fc391d6a2d92ba1518826c58a117
Eric Skaftason <skaftasoneric@gmail.com>
2024-10-10 16:17:34 -0400

    added test menu close button

--- index.js
+++ index.js
@@ -4,10 +4,17 @@ const callback_function = () => {
 
 import {Menu_Renderer} from './menu_renderer.js';
 
+const closeMenu = () => {
+    menu.close();   
+}
+
 const btns = [ //txt property must be an array
-    {txt: ['AA'], callback: callback_function},
+    {txt: ['AA'], callback: closeMenu},
     {txt: ['BB'], callback: callback_function},
     {txt: ['CC1', 'CC2', 'CC3'], callback: callback_function},
 ];
-const menu = new Menu_Renderer('Title 12345', 'Subtitle', 'version xyz', btns, 300, 600);
+
+const canvas = document.querySelector('.canvas');
+
+const menu = new Menu_Renderer('Title 12345', 'Subtitle', 'version xyz', btns, 300, 600, canvas);
 menu.init();
